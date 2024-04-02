025bc899a161bc79510bffb34507047d62adf795
lloorree <lore@chub.ai>
2024-04-01 22:58:26 -0400

    add DEFAULTS with explainer

--- src/TestRunner.tsx
+++ src/TestRunner.tsx
@@ -1,6 +1,6 @@
 import {ChubExtension} from "./ChubExtension.tsx";
 import {useEffect, useState} from "react";
-import {Extension, ExtensionResponse, InitialData} from "chub-extensions-ts";
+import {DEFAULT_INITIAL, DEFAULT_MESSAGE, Extension, ExtensionResponse, InitialData} from "chub-extensions-ts";
 
 // Modify this JSON to include whatever character/user information you want to test.
 import InitData from './assets/test-init.json';
@@ -16,8 +16,9 @@ export interface TestExtensionRunnerProps<ExtensionType extends Extension<StateT
 export const TestExtensionRunner = <ExtensionType extends Extension<StateType, ConfigType>,
     StateType, ConfigType>({ factory }: TestExtensionRunnerProps<ExtensionType, StateType, ConfigType>) => {
 
-    // @ts-ignore the linter doesn't always like the idea of reading types arbitrarily from strings
-    const [extension, _setExtension] = useState(new ChubExtension(InitData));
+    // You may need to add a @ts-ignore here,
+    //     as the linter doesn't always like the idea of reading types arbitrarily from files
+    const [extension, _setExtension] = useState(new ChubExtension({...DEFAULT_INITIAL, ...InitData}));
 
     // This is what forces the extension node to re-render.
     const [node, setNode] = useState(new Date());
@@ -39,23 +40,41 @@ export const TestExtensionRunner = <ExtensionType extends Extension<StateType, C
         refresh();
 
         const beforePromptResponse: Partial<ExtensionResponse<StateType>> = await extension.beforePrompt({
-            promptForId: "2",
-            anonymizedId: "0", content: "Hello, this is what happens when a human sends a message, but before it's sent to the model.", isBot: false
+            ...DEFAULT_MESSAGE, ...{
+                anonymizedId: "0",
+                content: "Hello, this is what happens when a human sends a message, but before it's sent to the model.",
+                isBot: false
+            }
         });
         console.assert(beforePromptResponse.error == null);
         refresh();
 
+        /***
+         "What is all of this nonsense with 'DEFAULT_MESSAGE'?" you may well ask.
+         The purpose of this is to future-proof your test runner.
+         The extension interface is designed to be forwards-compatible,
+            so that an extension with a certain library version will continue to work
+            even if new fields are added to any of the call/response objects.
+         But when new fields are added to the input objects, the code calling an
+            extension needs to be updated. Using DEFAULT_MESSAGE,
+            DEFAULT_INITIAL, DEFAULT_CHARACTER, DEFAULT_USER,
+            DEFAULT_LOAD_RESPONSE, and DEFAULT_RESPONSE
+            where relevant in your tests prevents a version bump
+            from breaking your test runner in many cases.
+         ***/
         const afterPromptResponse: Partial<ExtensionResponse<StateType>> = await extension.afterResponse({
+            ...DEFAULT_MESSAGE, ...{
             promptForId: null,
             anonymizedId: "2",
             content: "Why yes hello, and this is what happens when a bot sends a response.",
-            isBot: true});
+            isBot: true}});
         console.assert(afterPromptResponse.error == null);
         refresh();
 
         const afterDelayedThing: Partial<ExtensionResponse<StateType>> = await delayedTest(() => extension.beforePrompt({
+            ...DEFAULT_MESSAGE, ...{
             anonymizedId: "0", content: "Hello, and now the human is prompting again.", isBot: false, promptForId: null
-        }), 5);
+        }}), 5);
         console.assert(afterDelayedThing.error == null);
         refresh();
     }
