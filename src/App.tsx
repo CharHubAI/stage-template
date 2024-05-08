733d153e5eb51831e63b9df12176b5ce36591d40
lloorree <lore@chub.ai>
2024-05-08 07:52:34 -0400

    stages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import {ReactRunner} from "chub-extensions-ts";
-import {ChubExtension} from "./ChubExtension.tsx";
-import {TestExtensionRunner} from "./TestRunner.tsx";
+import {ReactRunner} from "@chub-ai/stages-ts";
+import {Stage} from "./Stage.tsx";
+import {TestStageRunner} from "./TestRunner.tsx";
 
 function App() {
   const isDev = import.meta.env.MODE === 'development';
   console.info(`Running in ${import.meta.env.MODE}`);
 
-  return isDev ? <TestExtensionRunner factory={ (data: any) => new ChubExtension(data) }/> :
-      <ReactRunner factory={(data: any) => new ChubExtension(data)} />;
+  return isDev ? <TestStageRunner factory={ (data: any) => new Stage(data) }/> :
+      <ReactRunner factory={(data: any) => new Stage(data)} />;
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   //    'always render twice in dev mode' convention,
   //     which is common enough to be worth leaving in
   //     commented-out code, but because of the nature
-  //     of extensions it's disabled by default here.
+  //     of stages it's disabled by default here.
   // <React.StrictMode>
     <App />
   // </React.StrictMode>,
