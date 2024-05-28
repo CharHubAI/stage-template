3fd0d9b8f903166a73a6ac51a2c9271326cf2bb1
lloorree <lore@chub.ai>
2024-05-28 17:29:03 -0400

    library build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,41 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), dts({
-        outDir: ['dist', 'types'],
-        include: ['src/index.ts', 'src/**/*.ts*'],
-        staticImport: true,
-        rollupTypes: true,
-        insertTypesEntry: true
-    })]
-})
+export default defineConfig(({ command, mode }) => {
+    if (mode != 'lib') {
+        return {
+            plugins: [react()]
+        }
+    } else {
+        return { plugins: [
+                react(),
+                dts({
+                    outDir: ['dist'],
+                    include: ['src/**/*.ts*'],
+                    staticImport: true,
+                    rollupTypes: true,
+                    insertTypesEntry: true,
+                }),
+            ],
+            build: {
+                lib: {
+                    entry: resolve(__dirname, 'src/index.ts'),
+                    name: 'index',
+                    formats: ['umd', 'es', 'cjs', 'iife'],
+                    fileName: 'index',
+                },
+                rollupOptions: {
+                    external: ['react', 'react-dom'],
+                    output: {
+                        globals: {
+                            react: 'React',
+                            'react-dom': 'ReactDOM',
+                        },
+                    },
+                }
+            }
+        }
+    }
+});
